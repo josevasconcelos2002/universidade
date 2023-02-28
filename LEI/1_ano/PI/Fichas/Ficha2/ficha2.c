ec6517f6dfe139c4f8897fb0d39ea9446d0ae1e3
José Vasconcelos <a100763@alunos.uminho.pt>
2023-02-28 11:35:55 +0000

    .

--- LEI/1_ano/PI/Fichas/Ficha2/ficha2.c
+++ LEI/1_ano/PI/Fichas/Ficha2/ficha2.c
@@ -10,6 +10,17 @@ float multInt1(int n, float m){
     return r;
 }
 
+float multInt2(int n, float m){
+    float r;
+    r = 0.0;
+    for(int i = 1;n>=1;i++){
+        if(n%2 == 1)r += m;
+        n/=2;
+        m*=2;
+    }
+    return r;
+}
+
 int main(){
     int n;
     float m;
@@ -18,7 +29,9 @@ int main(){
     printf("Introduza o valor de m: \n");
     int scan1 = scanf("%f",&m);
     if(scan && scan1){ 
-        float r = multInt1(n,m);
-        printf("Resultado: %f\n",r);
+        float r1 = multInt1(n,m);
+        float r2 = multInt2(n,m);
+        printf("Resultado 1: %f\n",r1);
+        printf("Resultado 2: %f\n",r2);
     }
 }
