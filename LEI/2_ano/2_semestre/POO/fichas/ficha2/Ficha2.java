9ae7221377c1d47b0b5967823b9c1138f5f7c944
José Vasconcelos <a100763@alunos.uminho.pt>
2023-03-03 09:09:23 +0000

    .

--- LEI/2_ano/2_semestre/POO/fichas/ficha2/Ficha2.java
+++ LEI/2_ano/2_semestre/POO/fichas/ficha2/Ficha2.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.String;
+import java.lang.reflect.Array;
 
 public class Ficha2{
     public static void main(String[] args) {
         int arr[] = new int[5];
+        int a,b;
         Exercicio1 ex1 = new Exercicio1();
         Scanner input = new Scanner(System.in);
         System.out.println("Introduza os elementos do array: ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(input.nextLine());
         }
+        //System.out.println("O menor elemento do array introduzido e: " + String.valueOf(ex1.minArray(arr)));
+        System.out.println("Introduza os indices:  ");
+        a = Integer.parseInt(input.nextLine());
+        b = Integer.parseInt(input.nextLine());
         input.close();
-        System.out.println("O menor elemento do array introduzido e: " + String.valueOf(ex1.minArray(arr)));
+        System.out.println("Array original:  " + Arrays.toString(arr));
+        System.out.println("Novo array:  " + Arrays.toString(ex1.newArray(arr, a, b)));
     }
 }
 
@@ -22,5 +30,16 @@ class Exercicio1{
             if(arr[i] < min) min = arr[i];
         }
         return min;
+    }
+
+    public int[] newArray(int arr[], int a, int b){
+        int j = 0;
+        int[] newArr = new int[b-a+1];
+        for(int i = a; i<=b ; i++){
+            newArr[j] = arr[i];
+            j++;
+        }
+        return newArr;
+        
     }
 }
---
+++ LEI/2_ano/2_semestre/SO/Fichas/Ficha1/ficha1.c
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int mycp(char *file_name, char *folder){
+    
+}
