83825d698f7c139986b2eff429f8096711820c0d
José Vasconcelos <a100763@alunos.uminho.pt>
2023-04-14 08:33:25 +0100

    Exercises from ficha 6

--- LEI/1_ano/PI/Fichas/Ficha6/ficha6.c
+++ LEI/1_ano/PI/Fichas/Ficha6/ficha6.c
@@ -1,18 +1,49 @@
 #define Max 100
 #include <stdio.h>
-
+#include <stdbool.h>
 
 struct staticStack {
     int sp;
     int values [Max];
 } STACK, *SStack;
 
-void SinitStack (SStack s){
-    s = NULL;
+void SinitStack (STACK *s){
+    if(s != NULL) s->sp = 0;
 }
 
-int SisEmpty (SStack s){
-    int r = 0;
-    if(s == NULL) r=1;
+int SisEmpty (STACK *s){
+    bool r = false;
+    if(s == NULL) r = true;
+    return (int)r;
+}
+
+int Spush(STACK *s, int x){
+    int r = 1;
+    if(s->sp < Max-1){
+        s->sp++;
+        s->values[s->sp] = x;
+        r = 0;
+    }
+    return r;
+}
+
+int Spop (STACK *s, int *x){
+    int r = 1;
+    if(!SisEmpty(s) == 0){
+        int N = s->values[s->sp];
+        s->sp--;
+        *x = N;
+        r = 1;
+    }
+    return r;
+}
+
+int Stop (STACK *s, int *x){
+    int r = 1;
+    if(!SisEmpty(s)){
+        int N = s->values[s->sp];
+        *x = N;
+        r = 0;
+    }
     return r;
 }
