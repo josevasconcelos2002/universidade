231a9c735705df8aa98a6c36c87bb07b3939d149
José Vasconcelos <a100763@alunos.uminho.pt>
2023-04-17 19:44:04 +0100

    .

--- LEI/2_ano/2_semestre/SO/Fichas/Ficha1/ficha1.c
+++ LEI/2_ano/2_semestre/SO/Fichas/Ficha1/ficha1.c
@@ -1,7 +1,22 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-int mycp(char *file_name, char *folder){
+
+int mycp(char *path){
+    int r = 0;
+    char buffer[4096] = {0};
+    int open1 = open(path,O_RDONLY,0660);
+    if(open1<0){
+        perror("Invalide path");
+        r = -1;
+        int close1 = close(open1);
+    }
+    return r;
+}
+
+int main(int argc,char *argv[]){
     
 }
