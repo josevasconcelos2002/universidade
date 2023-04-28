3b77a67e78c0616a20b6367c710753d2b485d3e1
José Vasconcelos <a100763@alunos.uminho.pt>
2023-04-28 15:52:13 +0100

    Ficha 7

--- LEI/1_ano/PI/Fichas/Ficha7/ficha7.c
+++ LEI/1_ano/PI/Fichas/Ficha7/ficha7.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct celula {
     char *palavra;
@@ -10,10 +11,11 @@ typedef struct celula {
 
 
 void libertaLista (Palavras p){
-    if(p!=NULL){
-        free(p->prox);
-        free(p->palavra);
-        free(p);
+    Palavras aux;
+    while(p != NULL){
+        aux = p;
+        p = p->prox;
+        free(aux);
     }
 }
 
@@ -30,7 +32,7 @@ int quantasP (Palavras l){
 int n_vezes(Palavras l){
     int count = 0;
     char *palavra = l->palavra;
-    while(l!=NULL && l->prox!=NULL){
+    while(l!=NULL){
         if(strcmp(palavra,l->palavra) == 0)
             count++;
         l = l->prox;
@@ -42,12 +44,55 @@ int n_vezes(Palavras l){
 void listaPal (Palavras l){
     while(l!=NULL && l->prox!=NULL){
         printf("%s %d\n",l->palavra,n_vezes(l));
+        l->ocorr = n_vezes(l);
         l = l->prox;
     }
     printf("%s %d\n",l->palavra,n_vezes(l));
+    l->ocorr = n_vezes(l);
 }
-
 char *ultima(Palavras l){
     while(l->prox != NULL) l = l->prox;
     return l->palavra;
+}
+
+Palavras acrescentaInicio (Palavras l, char *p){
+    Palavras aux = malloc(sizeof(Palavras));
+    aux->ocorr = 1;
+    aux->palavra = strdup(p);
+    aux->prox = l;
+    return aux;
+}
+
+Palavras acrescentaFim (Palavras l, char *p){
+    Palavras aux = malloc(sizeof(Palavras));
+    aux->ocorr = 1;
+    aux->palavra = strdup(p);
+    while(l->prox != NULL) l = l->prox;
+    l->prox = aux;
+    aux->prox = NULL;
+    return l;
+}
+Palavras acrescenta (Palavras l, char *p){
+    if(l != NULL){
+        while(l != NULL && strcmp(l->palavra,p)!=0) l = l->prox;
+        if(l != NULL) l->ocorr++;
+        else{
+            acrescentaFim(l,p);
+        }
+    }
+    else{
+        l = malloc(sizeof(Palavras));
+        l->palavra = strdup(p);
+        l->ocorr = 1;
+        l->prox = NULL;
+    }
+    return l;
+}
+struct celula * maisFreq (Palavras l){
+    struct celula *maisFreq = l;
+    while(l != NULL){
+        if(n_vezes(l) > n_vezes(maisFreq)) maisFreq = l;
+        l = l->prox;
+    }
+    return maisFreq;
 }
