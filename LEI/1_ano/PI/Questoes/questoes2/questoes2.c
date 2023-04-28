6625a18042760685413d486d12ae4d836697dcb3
José Vasconcelos <a100763@alunos.uminho.pt>
2023-04-28 16:57:51 +0100

    .

--- LEI/1_ano/PI/Questoes/questoes2/questoes2.c
+++ LEI/1_ano/PI/Questoes/questoes2/questoes2.c
@@ -7,6 +7,8 @@ typedef struct lligada {
     struct lligada *prox;
 } *LInt;
 
+
+// 1
 int length(LInt l){
     int length = 0;
     if(l!=NULL){
@@ -18,18 +20,50 @@ int length(LInt l){
     return length;
 }
 
+
+// 2
 void freeL(LInt l){
     LInt aux = malloc(sizeof(LInt));
     if(l!=NULL){
-       // ?
+       while(l != NULL){
+            aux = l;
+            l = l->prox;
+            free(aux);
+       }
     }
 }
 
+
+// 3
 void imprimeL(LInt l){
-    if(l){
-        while(l){
+    if(l != NULL){
+        while(l != NULL){
             printf("%d\n",l->valor);
             l = l->prox;
         }
+    }
+}
+
+// 5
+void insertOrd (LInt l, int x){
+    LInt aux = malloc(sizeof(LInt));
+    if(l != NULL){
+        while(l != NULL && x > l->valor) l = l->prox;
+        aux->valor = x;
+        if(l == NULL){
+            l = aux;
+            l->prox = NULL;
+        }
+        else{
+            LInt aux1;
+            aux1 = l->prox->prox;
+            l->prox = aux;
+            aux->prox = aux1;
+        }
+    }
+    else{
+        l = malloc(sizeof(LInt));
+        l->valor = x;
+        l->prox = NULL;
     }
 }
